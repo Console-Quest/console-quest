ca43a5f033f162e41e4b786092537b1712cd68c2
Anthony Keith <jt1keith@gmail.com>
2023-05-01 18:14:24 -0700

    added dot env config

--- gameServer.js
+++ gameServer.js
@@ -1,11 +1,14 @@
 'use strict'
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT = process.env.PORT || 3001;
 const KEY = process.env.OPENAI_API_KEY
+const ORG = process.env.ORG
 
 
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "YOUR_ORGANIZATION_AS_A_STRING_HERE",
+    organization: `${ORG}`,
     // Not sure this works, have to figure out how to use template literals
     apiKey: `${KEY}`,
 });
@@ -14,7 +17,7 @@ const openai = new OpenAIApi(configuration);
 // message.content is the prompt that sends to the API
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
-  messages: [{role: "user", content: "Pretend you are a text adventure game from the 80s, I need all responses to be 1 sentence, short and descriptive. i just stumbled into a room with a healing pool, describe it"}],
+  messages: [{role: "user", content: "Pretend you are a text adventure game from the 80s, I need all responses to be 1 sentence, short and descriptive. i just stumbled into a room full or riches and a dragon , can you describe it"}],
 });
 
 // This is just the response displayed as a string

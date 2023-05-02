2f6190a87d855a18173689838be13c990b6b5f43
Anthony Keith <jt1keith@gmail.com>
2023-05-01 17:46:22 -0700

    chat gpt api integrated. working as intended

--- gameServer.js
+++ gameServer.js
@@ -1,29 +1,25 @@
 'use strict'
-const { Server } = require('socket.io');
 const PORT = process.env.PORT || 3001;
-const io = new Server(PORT);
+const KEY = process.env.OPENAI_API_KEY
+
 
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "YOUR_ORG_ID",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    organization: "org-D3fgYbzDftFxkybTFSYwhUwE",
+    // Not sure this works, have to figure out how to use template literals
+    apiKey: `${KEY}`,
 });
 const openai = new OpenAIApi(configuration);
 
+// message.content is the prompt that sends to the API
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
-  messages: [{role: "user", content: "Hello world"}],
+  messages: [{role: "user", content: "Pretend you are a text adventure game from the 80s, I need all responses to be 1 sentence, short and descriptive. i just stumbled into a room with a healing pool, describe it"}],
 });
-console.log(completion.data.choices[0].message);
 
-io.on('connection', (socket) => {
-  console.log('A new user has connected');
-});
+// This is just the response displayed as a string
+console.log(completion.data.choices[0].message.content);
+
+
 
 console.log(`Listening on ${PORT}`);
